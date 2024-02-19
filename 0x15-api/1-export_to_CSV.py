45020daa2a5001c0a6d2b052fc5b8361f1eb41f3
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-20 00:15:39 +0200

    :"(

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,54 +1,35 @@
 #!/usr/bin/python3
-"""Retrieve and display employee TODO list progress."""
-import csv
+
+"""
+Fetches information about an employee and exports it to a CSV file.
+
+API: https://jsonplaceholder.typicode.com/
+"""
 import requests
 import sys
 
 API = "https://jsonplaceholder.typicode.com"
 
 
-def get_employee_info(employee_id):
-    response = requests.get(f"{API}/users/{employee_id}")
-    if not response.ok:
-        sys.exit()
-    return response.json()
-
-
-def get_employee_todos(employee_id):
-    response = requests.get(f"{API}/todos?userId={employee_id}")
-    if not response.ok:
-        sys.exit()
-    return response.json()
-
-
-def export_to_csv(employee_id, employee_info, employee_todos):
-    filename = f"{employee_id}.csv"
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME",
-                        "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for todo in employee_todos:
-            writer.writerow([employee_id, employee_info['username'],
-                            todo['completed'], todo['title']])
-    print(f"Data exported to {filename}")
-
-
-def main():
-    if len(sys.argv) != 2:
-        sys.exit()
-
-    employee_id = sys.argv[1]
-
-    try:
-        employee_id = int(employee_id)
-    except ValueError:
-        sys.exit()
-
-    employee_info = get_employee_info(employee_id)
-    employee_todos = get_employee_todos(employee_id)
-
-    export_to_csv(employee_id, employee_info, employee_todos)
-
-
 if __name__ == "__main__":
-    main()
+    employee_id = sys.argv[1]
+    response = requests.get("{}/users/{}".format(API, employee_id))
+    if not response.ok:
+        sys.exit()
+    employee_details = response.json()
+    response = requests.get("{}/todos?userId={}".format(API, employee_id))
+    if not response.ok:
+        sys.exit()
+    employee_todos = response.json()
+    username = employee_details.get('username')
+
+    CSV = ""
+    for todo in employee_todos:
+        title = todo.get('title')
+        status = todo.get('completed')
+        CSV += '"{}","{}","{}","{}"\n'.format(
+            employee_id, username, status, title
+        )
+
+    with open("{}.csv".format(employee_id), 'w') as csv_file:
+        csv_file.write(CSV)
