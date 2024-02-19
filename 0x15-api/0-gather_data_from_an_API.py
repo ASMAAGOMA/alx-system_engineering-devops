538a1d0a564787ae3c2312c86ead07b693713d6e
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-19 23:20:15 +0200

    :"(

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,20 +8,20 @@ API = "https://jsonplaceholder.typicode.com"
 def get_employee_info(employee_id):
     response = requests.get(f"{API}/users/{employee_id}")
     if not response.ok:
-        sys.exit("Failed to retrieve employee information.")
+        sys.exit()
     employee_info = response.json()
     return employee_info
 
 def get_employee_todos(employee_id):
     response = requests.get(f"{API}/todos?userId={employee_id}")
     if not response.ok:
-        sys.exit("Failed to retrieve employee TODO list.")
+        sys.exit()
     todos = response.json()
     return todos
 
 def main():
     if len(sys.argv) != 2:
-        sys.exit("Usage: python3 script.py <employee_id>")
+        sys.exit()
     
     employee_id = sys.argv[1]
     employee_info = get_employee_info(employee_id)
@@ -32,9 +32,10 @@ def main():
     num_completed_tasks = len(completed_tasks)
     
     print(f"Employee {employee_info['name']} is done with tasks"
-          f"({num_completed_tasks}/{total_tasks}):")
-    for i, task in enumerate(completed_tasks, start=1):
-        print(f"Task {i} Formatting: OK")
+      f"({num_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
 
 if __name__ == "__main__":
     main()
+
