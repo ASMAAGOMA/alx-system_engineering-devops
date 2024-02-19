4295b3328d356090770de40f7b087befdd1d33bc
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-19 23:29:52 +0200

    :"(

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-"""Retrieve and display employee TODO list progress."""
 import requests
 import sys
 
@@ -9,35 +7,39 @@ API = "https://jsonplaceholder.typicode.com"
 def get_employee_info(employee_id):
     response = requests.get(f"{API}/users/{employee_id}")
     if not response.ok:
-        sys.exit()
-    employee_info = response.json()
-    return employee_info
+        sys.exit("Failed to retrieve employee information.")
+    return response.json()
 
 
 def get_employee_todos(employee_id):
     response = requests.get(f"{API}/todos?userId={employee_id}")
     if not response.ok:
-        sys.exit()
-    todos = response.json()
-    return todos
+        sys.exit("Failed to retrieve employee TODO list.")
+    return response.json()
 
 
 def main():
     if len(sys.argv) != 2:
-        sys.exit()
+        sys.exit("Usage: python script_name.py EMPLOYEE_ID")
 
     employee_id = sys.argv[1]
+
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        sys.exit("Employee ID must be an integer.")
+
     employee_info = get_employee_info(employee_id)
     employee_todos = get_employee_todos(employee_id)
 
     completed_tasks = [todo for todo in employee_todos if todo['completed']]
-    total_tasks = len(employee_todos)
     num_completed_tasks = len(completed_tasks)
+    total_tasks = len(employee_todos)
 
-    print(f"Employee {employee_info['name']} is done with tasks"
-          f"({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_info['name']} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
+
 
 if __name__ == "__main__":
     main()
