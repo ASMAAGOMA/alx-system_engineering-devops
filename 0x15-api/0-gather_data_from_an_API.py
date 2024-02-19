9f3fc0a078cbb68e4bebe3fc821c53d6c28d49e8
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-19 23:21:38 +0200

    :"(

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,12 +5,14 @@ import sys
 
 API = "https://jsonplaceholder.typicode.com"
 
+
 def get_employee_info(employee_id):
     response = requests.get(f"{API}/users/{employee_id}")
     if not response.ok:
         sys.exit()
     employee_info = response.json()
     return employee_info
+
 
 def get_employee_todos(employee_id):
     response = requests.get(f"{API}/todos?userId={employee_id}")
@@ -19,18 +21,19 @@ def get_employee_todos(employee_id):
     todos = response.json()
     return todos
 
+
 def main():
     if len(sys.argv) != 2:
         sys.exit()
-    
+
     employee_id = sys.argv[1]
     employee_info = get_employee_info(employee_id)
     employee_todos = get_employee_todos(employee_id)
-    
+
     completed_tasks = [todo for todo in employee_todos if todo['completed']]
     total_tasks = len(employee_todos)
     num_completed_tasks = len(completed_tasks)
-    
+
     print(f"Employee {employee_info['name']} is done with tasks"
       f"({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
@@ -38,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
